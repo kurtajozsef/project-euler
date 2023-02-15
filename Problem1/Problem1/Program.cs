ea58a91a80fa4d176d51dff239a09a34a453bb3f
Jozsef Kurta <kurta_jozsef@yahoo.com>
2023-02-15 22:59:09 +0200

    Add solution for problems 1 and 2

---
+++ Problem1/Problem1/Program.cs
@@ -0,0 +1,11 @@
+﻿ulong sum = 0;
+
+for (ulong i = 1; i < 1000; i++)
+{
+	if (i % 3 == 0 || i % 5 == 0)
+	{
+		sum += i;
+	}
+}
+
+Console.WriteLine(sum);
---
+++ Problem2/Problem2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Program
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			ulong sum = 0;
+			ulong fibonacci = Fibonacci(1);
+			int i = 1;
+
+			while (fibonacci < 4000000)
+			{
+				if (fibonacci % 2 == 0)
+				{
+					sum += fibonacci;
+				}
+				i++;
+				fibonacci = Fibonacci(i);
+			}
+
+			Console.WriteLine(sum);
+		}
+
+		public static ulong Fibonacci(int number)
+		{
+			if (number == 0 || number == 1)
+			{
+				return 1;
+			}
+
+			return Fibonacci(number - 1) + Fibonacci(number - 2);
+		}
+	}
+}
