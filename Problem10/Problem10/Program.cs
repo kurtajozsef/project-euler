0c96b800317ce1ea3e9a75020bd872339893f0b1
Jozsef Kurta <kurta_jozsef@yahoo.com>
2023-02-16 21:14:27 +0200

    Add solution for problem 10

---
+++ Problem10/Problem10/Program.cs
@@ -0,0 +1,55 @@
+﻿//https://projecteuler.net/problem=10
+
+using System.Diagnostics;
+
+namespace Problem10
+{
+	public class Program
+	{
+		private static readonly List<long> StoredPrimes = new();
+
+		private static void GetAllPrimesSmallerThan(long n)
+		{
+			if (n <= 1)
+			{
+				return;
+			}
+
+			var wentOverN = false;
+			StoredPrimes.Add(2);
+			long i = 3;
+
+			while (!wentOverN)
+			{
+				if (IsPrime(i))
+				{
+					StoredPrimes.Add(i);
+				}
+
+				i += 2;
+				wentOverN = i > n;
+			}
+
+		}
+
+		public static long GetSumOfAllPrimesSmallerThan(long n)
+		{
+			GetAllPrimesSmallerThan(n);
+
+			return StoredPrimes.Sum();
+		}
+
+		private static bool IsPrime(long number) => StoredPrimes.Where(prime => prime <= Math.Sqrt(number)).All(prime => number % prime != 0);
+
+		public static void Main(string[] args)
+		{
+			var timer = Stopwatch.StartNew();
+
+			var result = GetSumOfAllPrimesSmallerThan(2_000_000);
+
+			timer.Stop();
+
+			Console.WriteLine($"Ans: {result}\nTime: {timer.ElapsedMilliseconds} ms");
+		}
+	}
+}
